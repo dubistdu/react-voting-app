6a190afc55eaba86444c959a5ef91709d7dd59fd
mama frantz <hyunfrantz@gmail.com>
2018-01-13 23:11:35 -0500

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello, friend! I am a basic React component.
+      </div>
+    );
+  }
+}
